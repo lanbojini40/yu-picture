9294bd6ef5c34ed149091d6861e3fd4179debc6c
lanbojini40 <1330399793@qq.com>
2024-12-18 20:17:11 +0800

    12/18

---
+++ src/main/java/com/yupi/yunpicturebackend/mapper/PictureMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.yunpicturebackend.mapper;
+
+import com.yupi.yunpicturebackend.model.entity.Picture;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 12600kf
+* @description 针对表【picture(图片)】的数据库操作Mapper
+* @createDate 2024-12-18 20:12:11
+* @Entity com.yupi.yunpicturebackend.model.entity.Picture
+*/
+public interface PictureMapper extends BaseMapper<Picture> {
+
+}
+
+
+
+
